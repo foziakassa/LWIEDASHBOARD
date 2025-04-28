44606c72060bb67bb07f8171b8448e98471d0fed
fozia <foziakassa19@gmail.com>
2025-04-28 12:21:44 +0300

    nn

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import { MainNav } from "@/components/main-nav"
 import { Search } from "@/components/search"
 import { UserNav } from "@/components/user-nav"
+import NotificationsPage from "./notification/page"
 
 export default function AdminLayout({
   children,
@@ -14,6 +15,10 @@ export default function AdminLayout({
       <div className="flex-1 flex flex-col min-h-screen">
         <header className="h-14 border-b bg-white dark:bg-gray-950 flex items-center px-4 justify-between sticky top-0 z-10">
           <Search />
+          {/* <div className="mt-6">
+          <NotificationsPage />
+
+          </div> */}
           <UserNav />
         </header>
         <main className="flex-1 p-4 overflow-auto bg-[#e5eded] dark:bg-gray-900">{children}</main>
